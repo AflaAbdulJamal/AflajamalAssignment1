301d2d3194868fdbfc893d30ffabb351533964fe
AflaAbdulJamal <aflaabdulj@gmail.com>
2024-02-02 11:37:16 +0530

    pushing assignment3

---
+++ cal.py
@@ -0,0 +1,12 @@
+a=int(input("enter first number"))
+b=int(input("enter first number"))
+op=input("enter operator")
+if(op=="+"):
+    c=a+b
+if(op=="-"):
+    c=a-b
+if(op=="*"):
+    c=a*b
+if(op=="/"):
+    c=a/b
+print(c)
